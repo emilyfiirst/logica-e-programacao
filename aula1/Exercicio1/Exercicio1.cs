2445d0e4ea8ad60306ae50117831ebd3bf01b271
emilyfiirst <emiliany20@live.com>
2026-03-11 09:16:45 -0400

    first commit

---
+++ aula1/Exercicio1/Exercicio1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Exercicio1
+{
+    static void Main()
+    {
+        float letraA = 0.21f;
+        int letraB = 1;
+        char letraC = 'X';
+        char letraD = '1';
+        string letraE = "José";
+        float letraF = 0.35f;
+        bool letraG = false;
+        double letraH = -0.0065651;
+        char letraI = 'D';
+        int letraJ = 3257;
+        char letraK = 'a';
+        string letraL = "+3257";
+        int letraM = 3257;
+    }
+}
